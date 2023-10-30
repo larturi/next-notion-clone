1cee0cce1fe36c0060946a65ab403c319680aaf5
Leandro Arturi <lea.arturi@gmail.com>
2023-10-30 16:21:14 -0300

    Delete and Replace cover image

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -10,6 +10,7 @@ import { Id } from "@/convex/_generated/dataModel";
 // import { Cover } from "@/components/cover";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Toolbar } from "@/components/toolbar";
+import { Cover } from "@/components/cover";
 
 interface DocumentIdPageProps {
   params: {
@@ -58,7 +59,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
 
   return (
     <div className="pb-60">
-      {/* <Cover url={document.coverImage} /> */}
+      <Cover url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-20">
         <Toolbar initialData={document} />
         {/* <Editor onChange={onChange} initialContent={document.content} /> */}
